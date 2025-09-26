d5263d769594d1fae19bb9fc406df22cd442b757
ryanm-plastic-recycling <ryanm@plastic-recycling.net>
2025-09-26 08:46:14 -0400

    Update startPM2log.bat

--- startPM2log.bat
+++ startPM2log.bat
@@ -10,8 +10,7 @@ REM Change to your project directory
 cd /d "C:\Users\ryanm\OneDrive - Plastic Recycling\Documents\GitHub\Maintenance-Dashboard"
 
 REM reload with any new .env vars
-pm2 reload ecosystem.config.cjs --update-env
-REM pm2 restart "maintenance-dashboard" --update-env
+pm2 reload ecosystem.config.cjs --update-env --env production 
 
 REM 1) PM2 process list
 start cmd /k "pm2 ls"
